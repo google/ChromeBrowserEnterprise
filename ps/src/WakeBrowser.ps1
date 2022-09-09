6722211dad2b20f796c8b14c199ea1a5316d9643
Niro S <92223536+nsamarakkody@users.noreply.github.com>
2022-09-09 15:19:01 -0500

    adding messages

--- ps/src/WakeBrowser.ps1
+++ ps/src/WakeBrowser.ps1
@@ -5,26 +5,28 @@ Install-Module -Name Invoke-CommandAs -Force
 #Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Google\Chrome -Name "CloudManagementEnrollmentToken"
 
 # Execute As System. Launch chrome browser as system in a temp data dir.
+Write-Output "Launch the browser"
 Invoke-CommandAs -ScriptBlock { 
     # set chrome user data dir.
     $chromeUserDataDir = "c:/temp3/cbcmLauncher"
-    Write-Output $chromeUserDataDir
+    #Write-Output $chromeUserDataDir
 
     # set a name for the chrome profile.
     $chromeProfile = "cbcmProfile"
-    Write-Output $chromeProfile
+    #Write-Output $chromeProfile
 
     $chromepaths = $chromeUserDataDir, $chromeUserDataDir + "\" + $chromeProfile
 
     # construct the chrome arg list.
     $chromeArgList =  "--user-data-dir=""" + $chromeUserDataDir + """ --profile-directory=""" + $chromeProfile + """"
-    Write-Output $chromeArgList
+    #Write-Output $chromeArgList
     Start-Process "chrome.exe" -ArgumentList $chromeArgList 
 } -AsSystem
 
 Start-Sleep -Seconds 15
 
 # Execute As System - Find chrome processes running as system and stop the process.
+Write-Output "Stop the browser"
 Invoke-CommandAs -ScriptBlock { 
     Get-Process -Name chrome -IncludeUserName | Where UserName -match system | Stop-Process -Force
 } -AsSystem
@@ -33,6 +35,7 @@ Invoke-CommandAs -ScriptBlock {
 Start-Sleep -Seconds 15
 
 # clean up chrome user data dir.
+Write-Output "Clean up work"
 $chromeUserDataDir = "c:/temp3"
 if (Test-Path -Path $chromeUserDataDir) {
     Remove-Item $chromeUserDataDir -Recurse
